40d39d24d6782c9237367b41e3f000b883e757aa
Kamugisha Bruce <brucekamugisha36@gmail.com>
2025-12-09 20:25:39 +0300

    build command to change to right directory

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Change to the Django project directory
+cd legal_digest
+
 # Collect static files
 python manage.py collectstatic --no-input
 
